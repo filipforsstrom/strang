519fa9e352f08c4d667c180aa1d843dbcee8b13d
filipforsstrom <filipforsstrom@gmail.com>
2023-10-29 15:36:21 +0100

    riff as module

--- lib/riff.lua
+++ lib/riff.lua
@@ -1,19 +1,50 @@
--- Define the GuitarRiff class
-local riff = class()
+local riff = {}
 
--- Define the constructor for the riff class
-function riff:init(notes, tempo)
-    riff.notes = notes
-    riff.tempo = tempo
-end
+function riff.init()
+    local a = {
+        triggers = {{
+            pulses = 2,
+            steps = 2,
+            rotation = 0,
+            sequins = s {}
+        }, {
+            pulses = 3,
+            steps = 4,
+            rotation = 0,
+            sequins = s {}
+        }, {
+            pulses = 3,
+            steps = 7,
+            rotation = 0,
+            sequins = s {}
+        }, {
+            pulses = 8,
+            steps = 8,
+            rotation = 0,
+            sequins = s {}
+        }},
+        amps = {s {1}, s {10.1}, s {100}, s {0.1}},
+        freqs = {s {50}, s {400, 300, 600}, s {50, 200, 400, 800}, s {1300, 1100, 1300, 1300}},
+        mutes = {s {16}, s {0.1}, s {0.5}, s {0.1}},
+        bends = {s {0}, s {10}, s {-10}, s {0}},
+        vibratos = {s {0}, s {0}, s {0.01}, s {0.1}}
+    }
 
--- Define the play method for the riff class
-function riff:set_position()
-    for i, note in ipairs(riff.notes) do
-        print(note)
-        os.execute("sleep " .. (60 / riff.tempo))
+    -- set triggers using er.gen and the values from er_tables
+    for i = 1, #a.triggers do
+        local pulses = a.triggers[i].pulses
+        local steps = a.triggers[i].steps
+        local rotation = a.triggers[i].rotation
+        local trigger = er.gen(pulses, steps, rotation)
+        a.triggers[i].triggers = trigger
     end
+
+    a.triggers[1].sequins:settable(a.triggers[1].triggers)
+    a.triggers[2].sequins:settable(a.triggers[2].triggers)
+    a.triggers[3].sequins:settable(a.triggers[3].triggers)
+    a.triggers[4].sequins:settable(a.triggers[4].triggers)
+
+    return a
 end
 
--- Return the riff class
 return riff
--- strang.lua
+++ strang.lua
@@ -1,5 +1,6 @@
 s = require 'sequins'
 er = require 'er'
+riff = include 'lib/riff'
 engine.name = 'Strang'
 
 play = true
@@ -8,48 +9,7 @@ play = true
 function init()
     engine.click_amp('all', 0.01)
     engine.amp('all', 10)
-    riff = {
-        triggers = {{
-            pulses = 2,
-            steps = 2,
-            rotation = 0,
-            sequins = s {}
-        }, {
-            pulses = 3,
-            steps = 4,
-            rotation = 0,
-            sequins = s {}
-        }, {
-            pulses = 3,
-            steps = 7,
-            rotation = 0,
-            sequins = s {}
-        }, {
-            pulses = 8,
-            steps = 8,
-            rotation = 0,
-            sequins = s {}
-        }},
-        amps = {s {1}, s {10.1}, s {100}, s {0.1}},
-        freqs = {s {50}, s {400, 300, 600}, s {50, 200, 400, 800}, s {1300, 1100, 1300, 1300}},
-        mutes = {s {16}, s {0.1}, s {0.5}, s {0.1}},
-        bends = {s {0}, s {10}, s {-10}, s {0}},
-        vibratos = {s {0}, s {0}, s {0.01}, s {0.1}}
-    }
-
-    -- set triggers using er.gen and the values from er_tables
-    for i = 1, #riff.triggers do
-        local pulses = riff.triggers[i].pulses
-        local steps = riff.triggers[i].steps
-        local rotation = riff.triggers[i].rotation
-        local trigger = er.gen(pulses, steps, rotation)
-        riff.triggers[i].triggers = trigger
-    end
-
-    riff.triggers[1].sequins:settable(riff.triggers[1].triggers)
-    riff.triggers[2].sequins:settable(riff.triggers[2].triggers)
-    riff.triggers[3].sequins:settable(riff.triggers[3].triggers)
-    riff.triggers[4].sequins:settable(riff.triggers[4].triggers)
+    riff1 = riff.init()
 
     trigger_step = s {1, 1, 2, 4, 1, 1, 3}
     current_trigger_step = 1
@@ -86,16 +46,16 @@ function iter()
         clock.sync(step_clock_speed)
         current_trigger_step = trigger_step()
         current_freq_step = freq_step()
-        pluck_clock_speed = riff.triggers[current_trigger_step].steps
+        pluck_clock_speed = riff1.triggers[current_trigger_step].steps
     end
 end
 
 function pluck()
     while true do
         clock.sync(1 / pluck_clock_speed)
-        local trig = riff.triggers[current_trigger_step].sequins()
+        local trig = riff1.triggers[current_trigger_step].sequins()
         if trig then
-            local freq = riff.freqs[current_freq_step]()
+            local freq = riff1.freqs[current_freq_step]()
             engine.trig(1, freq, 1)
             -- engine.trig(2, freq * (3 / 2), 1)
             -- engine.amp(current_step, 0.5)
@@ -108,7 +68,7 @@ function mute_clock()
     while true do
         clock.sync(1 / mute_clock_speed)
         current_mute_step = mute_step()
-        engine.string_decay('all', riff.mutes[current_mute_step]())
+        engine.string_decay('all', riff1.mutes[current_mute_step]())
     end
 end
 
@@ -116,7 +76,7 @@ function bend_clock()
     while true do
         clock.sync(1 / mute_clock_speed)
         current_bend_step = bend_step()
-        engine.bend_depth('all', riff.bends[current_bend_step]())
+        engine.bend_depth('all', riff1.bends[current_bend_step]())
     end
 end
 
@@ -124,7 +84,7 @@ function vibrato_clock()
     while true do
         clock.sync(1 / vibrato_clock_speed)
         current_vibrato_step = vibrato_step()
-        engine.vibrato_depth('all', riff.vibratos[current_vibrato_step]())
+        engine.vibrato_depth('all', riff1.vibratos[current_vibrato_step]())
     end
 end
 
@@ -132,7 +92,7 @@ function amp_clock()
     while true do
         clock.sync(1 / amp_clock_speed)
         current_amp_step = amp_step()
-        engine.amp('all', riff.amps[current_amp_step]())
+        engine.amp('all', riff1.amps[current_amp_step]())
     end
 end
 
